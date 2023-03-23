06df66751b97238e242e105dd9b179a42ce52cf4
JavaCplusUE <112417507+JavaCplusUE@users.noreply.github.com>
2023-03-23 12:35:25 +0500

    Update MyFirstGame.java

--- lesson_1/MyFirstGame.java
+++ lesson_1/MyFirstGame.java
@@ -1,19 +1,18 @@
 public class MyFirstGame {
-	public static void main(String[] args) {
-		System.out.println("Угадай число");
-		int randNum = 1;
-		int playerNum = 71;
-		int num = 1;
-	    while(playerNum != randNum){
-		    if (playerNum > randNum){
-		        	System.out.println("Число " + playerNum + " больше того, что загадал компьютер");
-		            playerNum--;
-		    } 
-		    if(playerNum < randNum){
-		            System.out.println("Число " + playerNum + " меньше того, что загадал компьютер"); 
-		            playerNum++;
-		    }
-		} 
-		System.out.println("Вы Победили!"); 		
-	}
+    public static void main(String[] args) {
+        System.out.println("Угадай число");
+        int secretNum = 1;
+        int playerNum = 71;
+        int num = 1;
+        while(playerNum != secretNum) {
+            if (playerNum > secretNum) {
+                System.out.println("Число " + playerNum + " больше того, что загадал компьютер");
+                playerNum--;
+            } else if (playerNum < secretNum) {
+                System.out.println("Число " + playerNum + " меньше того, что загадал компьютер");
+                playerNum++;
+            }
+        }
+        System.out.println("Вы Победили!");
+    }
 }
