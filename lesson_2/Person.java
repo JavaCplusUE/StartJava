40f4df85c62b099026973be316062b72c6879814
JavaCplusUE <JavaCplusUE@yandex.ru>
2023-04-01 22:59:20 +0500

    lesson_2/Person.java

--- lesson_2/Person.java
+++ lesson_2/Person.java
@@ -1,1 +1,28 @@
-Person
+public class Person {
+    String gender = "male";
+    String name = "Igor";
+    float height = 176.5f;
+    float weight = 94.4f;
+    int age = 32;
+
+    boolean move () {
+        return true;
+    }
+
+    void sit () {
+        System.out.println("sit");
+    }
+
+    void run () {
+        System.out.println("run");
+    }
+
+    String talk () {
+        return "talk";
+    }
+
+    void learnJava () {
+        System.out.println("learnJava");
+    }
+
+}
